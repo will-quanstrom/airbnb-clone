c8fa8f039c4e7802bb0958ad991b2cdd6d16886a
Jake Ash and Will Quanstrom <will@>
2020-02-12 16:16:44 -0800

    Make booking page styled correctly

--- src/components/CheckIn.jsx
+++ src/components/CheckIn.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function CheckIn() {
     const BoxMaker = {
-    marginLeft: '3000px',
+    marginLeft: '300px',
     marginTop: '200px',
     backgroundColor: '#FFFFFF',
     border: '15px #FFFFFF',
@@ -11,28 +11,42 @@ export default function CheckIn() {
     margin: '20px',
     position: 'absolute',
     borderRadius: '5px',
-    fontFamily: 'Circular, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif',
-    color: '#484848'
+    fontFamily: 'Circular',
+    color: '#484848',
+    boxShadow: '0 16px 40px rgba(0,0,0,0.12)'
     }
-
     const Inline = {
         display: 'inline-block'
+    }
+    const LabelChange = {
+        display: 'block'
+    }
+    const ButtonFloat = {
+        float: 'right'
     }
     return (
         <div style = {BoxMaker}>
             <h1>Book unique places to stay and things to do.</h1>
             <form>
-                <p>WHERE</p>
+                <label>WHERE</label>
+                <br></br>
                 <input type="text"></input>
+                <br/>
+                <br/>
                 <div style={Inline}>
-                    <p>CHECK-IN</p>
-                    <input type="text"></input>
-                    <p>CHECKOUT</p>
+                    <label style = {LabelChange}>CHECK-IN</label>
                     <input type="text"></input>
                 </div>
-                <p>Guests</p>
+                <div style={Inline}>
+                    <label style = {LabelChange}>CHECKOUT</label>
+                    <input type="text"></input>
+                </div>
+                <br/>
+                <br/>
+                <label>GUESTS</label>
+                <br/>
                 <input type="text"></input>
-                <button type="submit">Search</button>
+                <button type="submit" style = {ButtonFloat}>Search</button>
             </form>
 
         </div>
